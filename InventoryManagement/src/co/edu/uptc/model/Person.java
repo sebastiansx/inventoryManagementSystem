ad9cfa0950fd324c005ec76f40d3bc474251b196
sebastiansx <sebasgarzon610@gmail.com>
2025-05-15 23:14:54 -0500

    clases creadas

---
+++ InventoryManagement/src/co/edu/uptc/model/Person.java
@@ -0,0 +1,59 @@
+package co.edu.uptc.model;
+
+public abstract class Person {
+
+
+    private String name;
+    private String lastName;
+    private String id;
+    private String phone;
+    private String email;
+
+    public Person(String name, String lastName, String id, String phone, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
